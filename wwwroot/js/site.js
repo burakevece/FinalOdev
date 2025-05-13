058fb5ff759badd61c810d854356d3aeb2418f1b
Burak <mburakking19@gmail.com>
2025-05-13 11:33:09 +0300

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,24 +18,24 @@ $(document).ready(function () {
             },
             success: function (response) {
                 if (response.success) {
-                    // Başarılı işlem sonrası
+                    
                     if (response.message) {
                         showAlert('success', response.message);
                     }
 
-                    // Yönlendirme varsa
+                    
                     if (response.redirectUrl) {
                         setTimeout(function () {
                             window.location.href = response.redirectUrl;
                         }, 1500);
                     }
 
-                    // Tablo güncellemesi gerekiyorsa
+                   
                     if (response.updateTable) {
                         $('#ogrenciTablosu').html(response.tableHtml);
                     }
                 } else {
-                    // Hata durumunda
+                 
                     showAlert('danger', response.message || 'İşlem sırasında bir hata oluştu.');
                 }
             },
@@ -45,7 +45,7 @@ $(document).ready(function () {
         });
     });
 
-    // Öğrenci silme işlemi
+    
     $(document).on('click', '.ajax-delete', function (e) {
         e.preventDefault();
 
@@ -78,7 +78,7 @@ $(document).ready(function () {
         }
     });
 
-    // Detay görüntüleme (modal ile)
+    
     $(document).on('click', '.ajax-detail', function (e) {
         e.preventDefault();
 
@@ -98,22 +98,22 @@ $(document).ready(function () {
     });
 });
 
-// Alert mesajları gösterme fonksiyonu
+
 function showAlert(type, message) {
     var alertDiv = $('<div class="alert alert-' + type + ' alert-dismissible fade show" role="alert">' +
         message +
         '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' +
         '</div>');
 
-    // Sayfada önceden tanımlanmış alert container var mı kontrol et
+    
     if ($('#alertContainer').length) {
         $('#alertContainer').html(alertDiv);
     } else {
-        // Yoksa sayfanın üstüne ekle
+       
         $('.container:first').prepend(alertDiv);
     }
 
-    // 5 saniye sonra otomatik kapat
+    
     setTimeout(function () {
         alertDiv.alert('close');
     }, 5000);
